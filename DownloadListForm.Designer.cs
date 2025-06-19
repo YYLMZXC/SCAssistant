85fb331d626d6fd02fb34b9701a0324fb45303f9
YYLMZXC <YYLMZXC@163.com>
2025-06-20 00:31:16 +0800

    0.31

--- DownloadListForm.Designer.cs
+++ DownloadListForm.Designer.cs
@@ -2,7 +2,7 @@
 {
     partial class DownloadListForm
     {
-       
+
         private System.ComponentModel.IContainer components = null;
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
@@ -24,18 +24,21 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.listView1 = new System.Windows.Forms.ListView(); // ✅ 必须先初始化
+
+            this.listView1 = new System.Windows.Forms.ListView();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.openFolderMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.deleteRecordMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+
             // 
             // contextMenuStrip1
             // 
-            // contextMenuStrip1
             this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-        this.openFolderMenuItem,
-        this.deleteRecordMenuItem});
+            this.openFolderMenuItem,
+            this.deleteRecordMenuItem
+        });
             this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 70);
 
             // 
             // openFolderMenuItem
@@ -44,14 +47,19 @@
             this.openFolderMenuItem.Size = new System.Drawing.Size(180, 22);
             this.openFolderMenuItem.Text = "打开文件夹";
             this.openFolderMenuItem.Click += new System.EventHandler(this.openFolderMenuItem_Click);
+
+            // 
             // deleteRecordMenuItem
+            // 
             this.deleteRecordMenuItem.Name = "deleteRecordMenuItem";
             this.deleteRecordMenuItem.Size = new System.Drawing.Size(180, 22);
             this.deleteRecordMenuItem.Text = "删除记录";
             this.deleteRecordMenuItem.Click += new System.EventHandler(this.deleteRecordMenuItem_Click);
+
             // 
             // listView1
             // 
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
             this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listView1.FullRowSelect = true;
             this.listView1.Location = new System.Drawing.Point(0, 0);
@@ -59,7 +67,6 @@
             this.listView1.Size = new System.Drawing.Size(600, 400);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.ContextMenuStrip = this.contextMenuStrip1; // ✅ 在 listView 初始化之后设置
             this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
 
             // 
@@ -72,9 +79,9 @@
             this.Name = "DownloadListForm";
             this.Text = "下载文件列表";
             this.Load += new System.EventHandler(this.DownloadListForm_Load);
-            this.ResumeLayout(false);
         }
+    
 
-        #endregion
+    #endregion
     }
 }
