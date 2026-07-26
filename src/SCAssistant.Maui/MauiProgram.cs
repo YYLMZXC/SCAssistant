f13e67ee981c6f145d493fe9cadcbaf62c2d9dd2
yylmzxc <yylmzxc@163.com>
2026-07-26 12:31:23 +0800

    1

--- src/SCAssistant.Maui/MauiProgram.cs
+++ src/SCAssistant.Maui/MauiProgram.cs
@@ -1,6 +1,4 @@
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using SCAssistant.Maui.Services;
 
 namespace SCAssistant.Maui;
 
@@ -16,13 +14,6 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
-
-        builder.Services.AddSingleton<DownloadHistoryService>();
-        builder.Services.AddSingleton<DownloadManager>();
-        builder.Services.AddSingleton<CrossPlatformDownloadService>();
-
-        builder.Services.AddTransient<MainPage>();
-        builder.Services.AddTransient<DownloadListPage>();
 
 #if DEBUG
         builder.Logging.AddDebug();
