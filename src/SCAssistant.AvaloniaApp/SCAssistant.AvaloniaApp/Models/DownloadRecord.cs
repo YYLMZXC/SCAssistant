c6fa01ba669e31a4767ff7eff362ecf01e66fbd7
yylmzxc <yylmzxc@163.com>
2026-08-05 03:04:21 +0800

    1

--- src/SCAssistant.AvaloniaApp/SCAssistant.AvaloniaApp/Models/DownloadRecord.cs
+++
@@ -1,8 +0,0 @@
-namespace SCAssistant.AvaloniaApp.Models;
-
-public class DownloadRecord
-{
-    public string FileName { get; set; } = string.Empty;
-    public string Url { get; set; } = string.Empty;
-    public string LocalPath { get; set; } = string.Empty;
-}
--- src/SCAssistant.AvaloniaApp/SCAssistant.AvaloniaApp/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace SCAssistant.AvaloniaApp.ViewModels;
-
-public abstract class ViewModelBase : ObservableObject
-{
-}
